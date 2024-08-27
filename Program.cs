163fc9d57f9aee41a2054cf91f5163865b631869
khalid karam <khalid.tasks@gmail.com>
2024-08-27 23:45:37 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace HigherOrderFunction3
+{
+    internal class Program
+    {
+        //Example 3 Pipeline
+
+        static Func<int, int> DoubleFunc = x => x * 2;
+        static Func<int, int> SquareFunc = x => x * x;
+        static Func<int, int> IncrementFunc = x => x + 1;
+
+        static void Main()
+        {
+
+            Func<int, int> pipeline = x => IncrementFunc(SquareFunc(DoubleFunc(x)));
+
+            var input = 3;
+
+            var result = pipeline(input);
+
+            Console.WriteLine($"The result of processing {input} is {result}");
+        }
+
+    }
+}
